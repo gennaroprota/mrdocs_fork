6f20d96f4becb807f8b5116d456b3e0bd9ebf663
Gennaro Prota <gennaro.prota@gmail.com>
2025-06-20 11:51:24 +0200

    Add a missing include for std::free()

--- src/test_suite/detail/decomposer.cpp
+++ src/test_suite/detail/decomposer.cpp
@@ -1,5 +1,6 @@
 //
 // Copyright (c) 2023 Alan de Freitas (alandefreitas@gmail.com)
+// Copyright (c) 2025 Gennaro Prota (gennaro.prota@gmail.com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -8,6 +9,7 @@
 //
 
 #include "decomposer.hpp"
+#include <cstdlib>
 
 #if __has_include(<cxxabi.h>)
 #include <cxxabi.h>
